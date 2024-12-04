a429fd9b7c94018e5073e1ba72fad5aae9279538
ishanrana363 <ishanrana363@gmail.com>
2024-12-04 09:35:48 +0600

    cors added

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(hpp())
 // Using cors for enabling CORS
 
 app.use(cors({
-    origin: "",
+    origin: "https://final-project-backend-steel.vercel.app/api/v1",
     credentials: true
 }))
 
